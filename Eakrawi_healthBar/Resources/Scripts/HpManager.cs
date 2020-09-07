5210a309913d13285eaabb0edc17c3148330b82b
JohnsClever <eakrawichaisuriya@gmail.com>
2020-09-07 11:55:05 +0700

    V1.0.0

---
+++ Eakrawi_healthBar/Resources/Scripts/HpManager.cs
@@ -0,0 +1,75 @@
+﻿/*
+This package was made by Eakrawi Chaisuriya 6/9/2020 V1.3
+C# Unity2020.1.3f1
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpManager : MonoBehaviour
+{
+	[Header("Setting")]
+	public Camera main_cam;
+	[Tooltip("This will reduce performance. (Bad performance)")]
+	public bool isAutomatic;
+	[Header("Health Point Definition Components")]
+	public float maxHp;
+	public float hp;
+	[Header("Health Color Definition Components")]
+	public Gradient healthColorGradient;
+	[HideInInspector]
+	public Slider hpBarSlider;
+	[HideInInspector]
+	public Image hpBar;
+	private void Awake()
+	{
+		if (main_cam == null)
+		{
+			Debug.LogWarning("\"main_cam\" variable has not been assigned.");
+			main_cam = Camera.main;
+		}
+		else
+		{
+			GetComponent<Canvas>().worldCamera = main_cam;
+		}
+		hpBarSlider.maxValue = maxHp;
+		hp = maxHp;
+	}
+	/// <summary>
+	/// debug Hp
+	/// </summary>
+	public void ShowHp()
+	{
+		print(this.gameObject.name + " have " + hp + "Hp");
+	}
+	private void Update()
+	{
+		LookAtCamera();
+		if(isAutomatic)
+			UpdateHealthBarValue();
+	}
+	/// <summary>
+	/// Update value of the healthbar to have color and value corrected
+	/// </summary>
+	public void UpdateHealthBarValue()
+	{
+		hpBarSlider.value = hp;
+		hpBar.color = healthColorGradient.Evaluate(hp / maxHp);
+	}
+	/// <summary>
+	/// make canvas look at assigned camera
+	/// </summary>
+	public void LookAtCamera()
+	{
+		transform.LookAt(transform.position + main_cam.transform.forward);
+	}
+	/// <summary>
+	/// Change Hp to desired value
+	/// </summary>
+	/// <param name="hp"></param>
+	public void ChangeHp(float hp)
+	{
+		this.hp = hp;
+	}
+}
---
+++ Eakrawi_healthBar/Resources/Scripts/NamingAndLevel.cs
@@ -0,0 +1,42 @@
+﻿/*
+This package was made by Eakrawi Chaisuriya 6/9/2020 V1.3
+C# Unity2020.1.3f1
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NamingAndLevel : MonoBehaviour
+{
+	public string targetName = "Monster Name";
+	public int level = 5;
+	[HideInInspector]
+	public Text nameText;
+	[HideInInspector]
+	public Text levelText;
+
+	private void Awake()
+	{
+		RefreshSetting();
+	}
+	/// <summary>
+	/// Refresh name and level setting
+	/// </summary>
+	public void RefreshSetting()
+	{
+		nameText.text = targetName;
+		levelText.text = "Level : " + level;
+	}
+	/// <summary>
+	/// set the name and level of this instance
+	/// </summary>
+	/// <param name="targetName"></param>
+	/// <param name="targetLevel"></param>
+	public void SetNameAndLevel(string targetName, int targetLevel)
+	{
+		this.targetName = targetName;
+		this.level = targetLevel;
+		RefreshSetting();
+	}
+}
